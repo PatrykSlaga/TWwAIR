61f4a315db8de5f828db5dbd98b7922e5d60b592
PatrykSlaga <pat.slaga@wp.pl>
2025-04-09 11:06:37 +0200

    Add files via upload

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const config = {
     port: process.env.PORT || 3100,
-    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://Gordon:Kaczeta80!@cluster0.py0mv8z.mongodb.net/IoT?retryWrites=true&w=majority&appName=Cluster0'
+    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://username:password@cluster0.py0mv8z.mongodb.net/IoT?retryWrites=true&w=majority&appName=Cluster0'
  };
